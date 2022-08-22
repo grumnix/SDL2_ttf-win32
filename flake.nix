44f8233f6946ee2b21069c0bffab7c8ff7f9b737
Ingo Ruhnke <grumbel@gmail.com>
2022-08-22 19:14:18 +0200

    Initial checkin

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+
+    tinycmmc.url = "github:grumbel/tinycmmc";
+    tinycmmc.inputs.nixpkgs.follows = "nixpkgs";
+
+    SDL2_ttf_src.url = "https://github.com/libsdl-org/SDL_ttf/releases/download/release-2.20.1/SDL2_ttf-devel-2.20.1-mingw.tar.gz";
+    SDL2_ttf_src.flake = false;
+  };
+
+  outputs = { self, nixpkgs, tinycmmc, SDL2_ttf_src }:
+    tinycmmc.lib.eachWin32SystemWithPkgs (pkgs:
+      {
+        packages = rec {
+          default = SDL2_ttf;
+
+          SDL2_ttf = pkgs.stdenv.mkDerivation {
+            pname = "SDL2_ttf";
+            version = "2.20.1";
+
+            src = SDL2_ttf_src;
+
+            installPhase = ''
+              mkdir $out
+              cp -vr ${pkgs.stdenv.targetPlatform.config}/. $out/
+              substituteInPlace $out/lib/pkgconfig/SDL2_ttf.pc \
+                --replace "prefix=/opt/local/${pkgs.stdenv.targetPlatform.config}" \
+                          "prefix=$out"
+            '';
+          };
+        };
+      }
+    );
+}
