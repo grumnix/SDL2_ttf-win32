189fc88dc1cf5363dfde8d7474c9adf2ab1583a1
Ingo Ruhnke <grumbel@gmail.com>
2024-05-27 20:20:24 +0200

    Update to SDL2_ttf-2.22.0 and nixos-24.05

--- flake.nix
+++ flake.nix
@@ -1,11 +1,11 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-24.05";
 
     tinycmmc.url = "github:grumbel/tinycmmc";
     tinycmmc.inputs.nixpkgs.follows = "nixpkgs";
 
-    SDL2_ttf_src.url = "https://github.com/libsdl-org/SDL_ttf/releases/download/release-2.20.1/SDL2_ttf-devel-2.20.1-mingw.tar.gz";
+    SDL2_ttf_src.url = "https://github.com/libsdl-org/SDL_ttf/releases/download/release-2.22.0/SDL2_ttf-devel-2.22.0-mingw.tar.gz";
     SDL2_ttf_src.flake = false;
   };
 
@@ -17,7 +17,7 @@
 
           SDL2_ttf = pkgs.stdenv.mkDerivation {
             pname = "SDL2_ttf";
-            version = "2.20.1";
+            version = "2.22.0";
 
             src = SDL2_ttf_src;
 
